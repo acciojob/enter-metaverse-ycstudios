c09536fce5c06368d0987bbd45ee23bef216d997
Yash Chandanshive <66194004+ycstudios@users.noreply.github.com>
2025-02-20 22:26:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+document.getElementById('enterBtn').addEventListener('click',()=>{
+    const status=document.getElementById('status');
+    if(status.textContent==='Enter the Metaverse'){
+     status.innerHTML=`<h1>Entered Metaverse</h1>`   
+    }
+    
+
+})
